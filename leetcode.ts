f8759f239e5d18807b8a655dba04da3f5b61771c
buivangiaphatdev <buivangiaphat@gmail.com>
2025-04-17 11:18:56 +0700

    leet code 20, 242

---
+++ leetcode.ts
@@ -0,0 +1,17 @@
+//===========leetcode 21 ===================
+// const list1:number[] = [1,2,4];
+// const list2:number[] = [1,3,4];
+
+// let list:number[] = list1.concat(list2)
+// list.sort();
+// console.log(list);
+
+//===========leetcode 2 ====================
+// const list1:number[]=[2,4,3];
+// const list2:number[]=[5,6,4];
+
+// const num:number = Number(list1.reverse().join(''))+Number(list2.reverse().join(''));
+// const list:number[]=(num.toString().split('').map(Number)).reverse();
+// console.log(list)
+
+//===========leetcode 7 Reverse Integer ================
---
+++ leetcode242.ts
@@ -0,0 +1,36 @@
+function isAnagram1(str1: string, str2: string): boolean {
+  if (str1.length != str2.length) return false;
+
+  const map = new Map<string, number>();
+
+  for (let i = 0; i < str1.length; i++) {
+    const charStr1: string = str1[i];
+    const charStr2: string = str2[i];
+
+    map.set(charStr1, (map.get(charStr1) || 0) + 1);
+    map.set(charStr2, (map.get(charStr2) || 0) - 1);
+  }
+  for (let [key, value] of map) {
+    if (value !== 0) return false;
+  }
+  return true;
+}
+
+function isAnagram2(str1: string, str2: string):boolean {
+  if (str1.length != str2.length) return false;
+
+  const fre: number[] = new Array(26).fill(0);
+
+  for(let i=0; i<str1.length;i++){
+    let index = str1.charCodeAt(i) - 'a'.charCodeAt(0);
+    fre[index]++;
+  }
+
+  for(let i=0; i<str2.length;i++){
+    let index = str2.charCodeAt(i)-'a'.charCodeAt(0);
+    if(fre[index] ==0) return false;
+    fre[index]--;
+  }
+  return true;
+}
+console.log(isAnagram2("anagram", "nagaram"));
