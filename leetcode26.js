80f596c518b8cd3ac68389ea7ae7339459ad1a12
buivangiaphatdev <buivangiaphat@gmail.com>
2025-04-20 23:20:08 +0700

    update code js

--- leetcode26.js
+++ leetcode26.js
@@ -13,27 +13,26 @@
 //             i++;
 //         }
 //         j++;
-//      }   
+//      }
 //      return i;
 //     }
 
 //=========================End-Java====================
 
-var removeDuplicates = function(nums) {
-    const n=nums.length;
+var removeDuplicates = function (nums) {
+  const n = nums.length;
 
-    if (n===0 ||n===1) return n;
+  if (n === 0 || n === 1) return n;
 
-    let i=1; j=1;
-    while(j<n){
-        if(nums[j]!==nums[j-1]){
-            nums[i]===nums[j];
-            i++;
-        }
-        j++;
+  let i = 1;
+  for (let j = 1; j < n; j++) {
+    if (nums[j] !== nums[j - 1]) {
+      nums[i] = nums[j];
+      i++;
     }
-    return i;
-}
+  }
+  return i;
+};
 
-console.log(removeDuplicates([1,1,2])) // 2
-console.log(removeDuplicates([0,0,1,1,2,2,3,3,4])) // 5
+console.log(removeDuplicates([1, 1, 2])); // 2
+console.log(removeDuplicates([0, 0, 1, 1, 2, 2, 3, 3, 4])); // 5
